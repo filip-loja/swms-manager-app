4b0b9cc107b6bdf6ab90dda3cf819870edd04f1c
Filip Loja <filiploja@gmail.com>
2021-05-23 05:01:51 +0200

    App init and drawer design

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+
+  </q-page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'PageIndex',
+  components: { },
+  data() {
+    return { };
+  }
+});
+</script>
